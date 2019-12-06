2308679d5e5f5ae78521dd770ea07e5fa0b79a2a
jtCodes <jt234wq@gmail.com>
2019-12-06 12:27:07 -0500

    adding tablebundle to handle data filtering by time

--- src/lib/Table.js
+++ src/lib/Table.js
@@ -1,140 +1,134 @@
 import React, { PureComponent } from "react";
-import { format } from 'date-fns';
-import "./Table.css"
+import { format } from "date-fns";
+import "./Table.css";
 
 // data = [{time: int, measurement1: int, measurement2: int...}]
 // key = [time, measurement1, meansurement2...]
 class Table extends PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-        this.tableKeys = this.props.data.keys
-        this.tableData = this.props.data.data
+  measurementOnClick = e => {
+    let measurementData = [];
+
+    // update the store
+    this.tableData.map(data => {
+      measurementData.push({
+        time: data["time"],
+        value: data[e.target.dataset.rowlabel]
+      });
+    });
+
+    this.props.updateTableState({
+      name: e.target.dataset.rowlabel,
+      data: measurementData
+    });
+  };
+
+  colOnClick = e => {
+    let classNames = e.target.className.split(" ");
+    let selectedCol1 = Number(classNames[1]);
+    let selectedColIndex = parseInt(classNames[2]);
+    let neighborColIndex;
+
+    if (this.tableData.length === 1) {
+      neighborColIndex = 0;
+    } else if (selectedColIndex === this.tableData.length - 1) {
+      neighborColIndex = selectedColIndex - 1;
+    } else {
+      neighborColIndex = selectedColIndex + 1;
     }
 
-    measurementOnClick = (e) => {
-        let measurementData = []
+    this.props.updateTableState({
+      selectedCol1: selectedCol1,
+      selectedCol2: this.tableData[neighborColIndex].time
+    });
+  };
 
-        // update the store
-        this.tableData.map(data => {
-            measurementData.push({
-                time: data["time"],
-                value: data[e.target.dataset.rowlabel]
-            })
-        })
-
-        this.props.updateTableState({
-            name: e.target.dataset.rowlabel,
-            data: measurementData
-        })
+  render() {
+    if (!this.props.data) {
+      return null;
     }
 
-    colOnClick = (e) => {
-        let classNames = e.target.className.split(" ")
-        let selectedCol1 = Number(classNames[1])
-        let selectedColIndex = parseInt(classNames[2])
-        let neighborColIndex
+    let { selectedCol1, selectedCol2 } = this.props;
+    this.tableKeys = this.props.data.keys;
+    this.tableData = this.props.data.data;
 
-        if (this.tableData.length === 1) {
-            neighborColIndex = 0
-        } else if (selectedColIndex === this.tableData.length - 1) {
-            neighborColIndex = selectedColIndex - 1
-        } else {
-            neighborColIndex = selectedColIndex + 1
-        }
+    const TableBody = ({ measurements }) => {
+      {
+        return measurements.map((m, i) => {
+          return <TableRow key={m} m={m} rowIndex={i} />;
+        });
+      }
+    };
 
-        this.props.updateTableState({
-            selectedCol1: selectedCol1,
-            selectedCol2: this.tableData[neighborColIndex].time
-        })
-    }
+    // creates the table row by row
+    const TableRow = ({ m, rowIndex }) => (
+      <tr
+        key={m + 1}
+        style={{
+          background: rowIndex % 2 === 0 ? "#EEEEEE" : "white"
+        }}
+      >
+        {// first cell in the row is measurement label
+        m === "time" ? (
+          <td className={"folder-table-" + m} key={m}></td>
+        ) : (
+          <td
+            className={`${"folder-table-" + m} ${"firsttd"}`}
+            data-rowlabel={m}
+            key={m}
+            onClick={this.measurementOnClick}
+          >
+            {m}
+          </td>
+        )}
+        {this.tableData.map((data, colIndex) => {
+          let curTime = this.tableData[colIndex].time;
+          let cellText;
 
-    render() {
-        if (!this.props.data) {
-            return null;
-        }
+          if (rowIndex === 0) {
+            let date = new Date(curTime * 1000);
+            cellText = format(date, "hh:MMA");
+          } else {
+            cellText = data[m];
+          }
 
-        this.tableKeys = this.props.data.keys
-        this.tableData = this.props.data.data
+          return (
+            <td
+              className={`${"folder-table-" + m} ${curTime} ${colIndex + "td"}`}
+              data-unixtime={curTime}
+              key={colIndex}
+              onClick={this.colOnClick}
+              style=// double ternary statements needed because the first row's
+              // initial background is different than the rest
+              {
+                selectedCol1 === curTime || selectedCol2 === curTime
+                  ? { background: "rgba(247, 173, 229, 0.3)" }
+                  : {}
+              }
+            >
+              {cellText}
+            </td>
+          );
+        })}
+      </tr>
+    );
 
-        const TableBody = ({ measurements }) => {
-            {
-                return (
-                    measurements.map((m, i) => {
-                        return (
-                            <TableRow key={m} m={m} rowIndex={i} />
-                        )
-                    })
-                )
-            }
-        }
-
-        // creates the table row by row
-        const TableRow = ({ m, rowIndex }) => (
-            <tr
-                key={m + 1}
-                style={{
-                    background: rowIndex % 2 === 0 ? "#EEEEEE" : "white"
-                }}>
-                {   // first cell in the row is measurement label
-                    m === "time" ?
-                        <td className={"folder-table-" + m} key={m}></td> :
-                        <td className={`${"folder-table-" + m} ${"firsttd"}`}
-                            data-rowlabel={m}
-                            key={m}
-                            onClick={this.measurementOnClick}
-                        >
-                            {m}
-                        </td>
-                }
-                {this.tableData.map((data, colIndex) => {
-                    let curTime = this.tableData[colIndex].time
-                    let cellText
-
-                    if (rowIndex === 0) {
-                        let date = new Date(curTime * 1000)
-                        cellText = format(date, "hh:MMA")
-                    } else {
-                        cellText = data[m]
-                    }
-
-                    return (
-                        <td
-                            className={`${"folder-table-" + m} ${curTime} ${colIndex + "td"}`}
-                            data-unixtime={curTime}
-                            key={colIndex}
-                            onClick={this.colOnClick}
-                            style=
-                            // double ternary statements needed because the first row's 
-                            // initial background is different than the rest
-                            {this.props.selectedCol1 === curTime ||
-                                this.props.selectedCol2 === curTime ?
-                                { background: "rgba(247, 173, 229, 0.3)" } : {}}
-                        >
-                            {cellText}
-                        </td>
-                    )
-                })}
-            </tr>
-        )
-
-        return (
-            <div className="folder-table-container">
-                <div
-                    className="folder-table"
-                    style={{ overflowX: "auto" }}
-                >
-                    <table key="folder-table">
-                        <tbody key="folder-table-body">
-                            <TableBody measurements={this.tableKeys} />
-                        </tbody>
-                    </table>
-                    <br style={{ clear: "both" }} />
-                </div>
-            </div>
-        );
-
-    }
+    return (
+      <div className="folder-table-container">
+        <div className="folder-table" style={{ overflowX: "auto" }}>
+          <table key="folder-table">
+            <tbody key="folder-table-body">
+              <TableBody measurements={this.tableKeys} />
+            </tbody>
+          </table>
+          <br style={{ clear: "both" }} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Table;
---
+++ src/lib/TableBundle.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from "react";
+import Table from "./Table";
+
+class TableBundle extends PureComponent {
+  render() {
+    const {
+      data,
+      dtWindow,
+      updateTableState,
+      selectedMeasurement,
+      selectedCol1,
+      selectedCol2
+    } = this.props;
+
+    let filteredData = data.filter(d => {
+      return d["time"] >= dtWindow[0] && d["time"] <= dtWindow[1];
+    });
+
+    return (
+      <Table
+        data={filteredData}
+        updateTableState={updateTableState}
+        selectedMeasurement={selectedMeasurement}
+        selectedCol1={selectedCol1}
+        selectedCol2={selectedCol2}
+      />
+    );
+  }
+}
+
+export default TableBundle;
